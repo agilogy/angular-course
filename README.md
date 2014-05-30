69923f14c0f4aed4fb512046388889267b4931b3
Sergio Gimeno <gimeno.sergio@gmail.com>
2014-05-30 12:19:28 +0200

    Initial commit. Updated docs.

---
+++ README.md
@@ -0,0 +1,21 @@
+### Setup
+
+```
+> git clone <this repo>
+> npm install
+> bower install
+```
+
+### Develop
+
+```
+> grunt serve
+```
+
+### Test
+
+```
+> grunt test
+
+``
+
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('angularCourseApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'Agilogy'
+    ];
+  });
