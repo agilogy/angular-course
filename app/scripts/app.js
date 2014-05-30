94ece0047b359693d59ddae89efcb487a301d271
Sergio Gimeno <gimeno.sergio@gmail.com>
2014-05-30 16:51:18 +0200

    Use angular-strap scrollspy directive.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,17 +1,23 @@
 'use strict';
 
 angular
-  .module('angularCourseApp', [
-    'ngSanitize',
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+.module('angularCourseApp', [
+  'ngSanitize',
+  'ngRoute',
+  'mgcrea.ngStrap'
+])
+
+.value('$anchorScroll', angular.noop)
+
+.config(function () {
+  // $routeProvider
+  //   .when('/', {
+  //     templateUrl: 'views/main.html',
+  //     controller: 'MainCtrl'
+  //   })
+  //   .otherwise({
+  //     redirectTo: '/'
+  //   });
+})
+
+;
